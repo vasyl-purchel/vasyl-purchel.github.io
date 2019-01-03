ddc657c752a8ffa76f4182d92d346b7da8f71201
Vasyl Purchel <vasyl.purchel@workday.com>
2019-01-03 16:29:05 +0000

    Updating source page link
    Change-Id: Ie64fc788ba27c3413ee0448c57cfff11ebf73e3e

--- src/Global/Constants.fs
+++ src/Global/Constants.fs
@@ -1,3 +1,3 @@
 module Constants
 
-let GithubPage = @"https://github.com/vasyl-purchel/vasyl-purchel.github.io"
+let GithubPage = @"https://github.com/vasyl-purchel/vasyl-purchel.github.io/tree/src"
